625bbe7cdd04af72cd2630f87203b2ba0b91ccc5
Benas Grigalauskas <73246949+BenasGri@users.noreply.github.com>
2020-12-21 20:43:59 +0200

    surašytos visos duotos reikšmės ir funkcijos

--- BenasGrigalauskas_3lab.m
+++ BenasGrigalauskas_3lab.m
@@ -2,3 +2,21 @@ clear all;
 close all;
 clc;
 
+%Iejimo vektorius
+x = 0.1:1/22:1;
+
+%Norimo atsako funkcija
+y = (1 + 0.6*sin(2*pi*x/0.7)) + 0.3*sin(2*pi*x))/2;
+
+%Centru reiksmes
+c1 = 0.2;
+c2 = 0.8;
+
+%Spinduliu reiksmes
+r1 = 0.4;
+r2 = 0.6;
+
+
+function [F] = gauss(x, c, r) 
+    F = exp(-(x-c)^2/(2*r^2));
+end;
